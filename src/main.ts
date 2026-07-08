ce2f58a0033c7147c962e5c804531f313f9c959e
ayahs94 <137565573+ayahs94@users.noreply.github.com>
2026-07-09 00:36:15 +0200

    Register IonicVue plugin without UI migration

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,12 @@ import Aura from '@primevue/themes/aura'
 import 'primeicons/primeicons.css'
 import App from './App.vue'
 import router from './router'
-
+import { IonicVue } from '@ionic/vue'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(IonicVue)
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
